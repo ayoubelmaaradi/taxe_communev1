2b18c65c83f1d2df1a1a7bebb21b6ede73334219
ayoubelmaaradi <ayoub1913@outlokk.com>
2017-03-07 23:14:08 +0000

    admin commits

--- src/java/bean/AnnexeAdministratif.java
+++ src/java/bean/AnnexeAdministratif.java
@@ -21,8 +21,6 @@ import javax.persistence.OneToMany;
 @Entity
 public class AnnexeAdministratif implements Serializable {
 
-    
-
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
